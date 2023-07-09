9a21d067c7a7b525b80fd02d1cfd85d4093699c1
Raymond Pee Yong Nging <raymondpyn@gmail.com>
2023-07-09 21:12:56 +0800

    oneapi-cuda onboarding

---
+++ src/sycl-init/simple-sycl-app.cpp
@@ -0,0 +1,46 @@
+#include <sycl/sycl.hpp>
+
+int main() {
+  // Creating buffer of 4 ints to be used inside the kernel code
+  sycl::buffer<sycl::cl_int, 1> Buffer(4);
+
+  // Creating SYCL queue
+  sycl::queue Queue;
+
+  // Size of index space for kernel
+  sycl::range<1> NumOfWorkItems{Buffer.size()};
+
+  // Submitting command group(work) to queue
+  Queue.submit([&](sycl::handler &cgh) {
+    // Getting write only access to the buffer on a device
+    auto Accessor = Buffer.get_access<sycl::access::mode::write>(cgh);
+    // Executing kernel
+    cgh.parallel_for<class FillBuffer>(
+        NumOfWorkItems, [=](sycl::id<1> WIid) {
+          // Fill buffer with indexes
+          Accessor[WIid] = (sycl::cl_int)WIid.get(0);
+        });
+  });
+
+  // Getting read only access to the buffer on the host.
+  // Implicit barrier waiting for queue to complete the work.
+  const auto HostAccessor = Buffer.get_access<sycl::access::mode::read>();
+
+  // Check the results
+  bool MismatchFound = false;
+  for (size_t I = 0; I < Buffer.size(); ++I) {
+    if (HostAccessor[I] != I) {
+      std::cout << "The result is incorrect for element: " << I
+                << " , expected: " << I << " , got: " << HostAccessor[I]
+                << std::endl;
+      MismatchFound = true;
+    }
+  }
+
+  if (!MismatchFound) {
+    std::cout << "The results are correct!" << std::endl;
+  }
+
+  return MismatchFound;
+}
+
